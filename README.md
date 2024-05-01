80fcac0ce7af0939bb596d38d0a22861b7c160ab
Aditiyo7 <aditiyo.nugroho7@gmail.com>
2024-05-02 00:51:55 +0700

    Homework Web Automation Fundamental

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # wdio-digitalscola
+## Homework Web Automation Fundamental
+
+### From Sample Test [www.saucedemo.com](www.saucedemo.com)
+
+1. User success login
+2. Validate user in dashboard page after login
+3. Add item to cart
+4. Validate item successfully added to cart
+
---
+++ test/specs/test.specs.js
@@ -0,0 +1,32 @@
+
+
+describe('Homework Web Automation', () => {
+    it('Test Case 1 - Success', async () => {
+        await browser.url('https://www.saucedemo.com/')
+        
+        const username = await browser.$('#user-name')
+        const password = await browser.$('#password')
+        const loginButton = await browser.$('#login-button')
+
+        await username.setValue('standard_user')
+        await password.setValue('secret_sauce')
+
+        console.log(`Value dari button ${await loginButton.getValue()}`)   
+        await loginButton.click()
+        
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+
+        const addTocart = await browser.$('#add-to-cart-sauce-labs-backpack')
+        const shoppingCart = await browser.$('#shopping_cart_container')
+
+        await addTocart.click()
+        await shoppingCart.click()
+
+        const elem = await $('//*[@data-test="item-quantity"]')
+        await expect(elem).toHaveText('1')
+
+
+        await browser.pause(1000)    
+    })
+
+})
